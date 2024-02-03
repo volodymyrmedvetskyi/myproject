b19d1dae2d976add806c19e5117c7163ce9f1f8d
volodymyrmedvetskyi <volodymyrmedvetskyi@gmail.com>
2024-02-04 00:35:30 +0200

    Revert "first commit"
    This reverts commit 4c2d5a7aadec4d8470da918b6891769f543bb01b.

--- application/terraform-app/modules/server/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_id_application_server" {
-  value = aws_instance.application_server.id
-}
-
-output "public_address_application_server" {
-  value = aws_instance.application_server.public_ip
-}
