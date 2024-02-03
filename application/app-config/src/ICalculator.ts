e72eee6ab1b02a7ab7ca468c80103acf8d4bc1b1
volodymyrmedvetskyi <volodymyrmedvetskyi@gmail.com>
2024-02-04 00:56:56 +0200

    deleted

--- application/app-config/src/ICalculator.ts
+++
@@ -1,8 +0,0 @@
-// src/ICalculator.ts
-
-export interface ICalculator {
-  add(a: number, b: number): number;
-  subtract(a: number, b: number): number;
-  multiply(a: number, b: number): number;
-  divide(a: number, b: number): number;
-}
